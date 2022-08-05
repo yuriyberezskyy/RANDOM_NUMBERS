fd613f24c8d7ed8a5de789466913ba030bcb7709
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-05 00:20:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+#Integer
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#Float numbers
+random_float  = random.random()
+print(random_float)
+
+#Float from 0.00000 to 5.00000
+print(random_float * 5)
