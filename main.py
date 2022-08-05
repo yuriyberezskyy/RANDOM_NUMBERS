7a1c319f9469a93c7f5813fbdf9a6de969100938
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-05 00:41:24 +0000

    List and methods

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ print(random_float)
 
 #Float from 0.00000 to 5.00000
 print(random_float * 5)
+
+states_of_america = ["Delaware", "Pennsylvania","New Jersey"]
+
+states_of_america.append("New York")
+states_of_america.extend(["California","Oregon"])
+print(states_of_america)
+
